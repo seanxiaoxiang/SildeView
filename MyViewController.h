92d2ff84ad7b18ecdac67db030e459bd5d19d3b2
seanxiaoxiang <seanxiaoxiang@gmail.com>
2013-07-30 10:38:40 +0800

    新增MyViewController

---
+++ MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  SlideView
+//
+//  Created by admin on 13-7-30.
+//  Copyright (c) 2013年 wesleyxiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
